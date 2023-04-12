99c6a98ce444f2cabd4d8c977f01e8625cccceac
Anatoliy Katelnitskiy <katel1998@yandex.ru>
2023-04-12 16:44:49 +0300

    add remove task button

--- src/components/trelloBoard.Component/trelloBoard.Component.js
+++ src/components/trelloBoard.Component/trelloBoard.Component.js
@@ -1,25 +1,23 @@
-import './trelloBoard.component.css'
-import TrelloColumn from '../trelloColumn.Component/trelloColumn.Component';
+import "./trelloBoard.component.css";
+import TrelloColumn from "../trelloColumn.Component/trelloColumn.Component";
 export default class TrelloBoard {
   constructor(containerEl) {
     this.containerEl = containerEl;
   }
 
   drowUi() {
-    const trelloBoardEl = document.createElement('div');
-    trelloBoardEl.classList.add('trello-board');
+    const trelloBoardEl = document.createElement("div");
+    trelloBoardEl.classList.add("trello-board");
 
-    const columnTodo = new TrelloColumn(trelloBoardEl, 'ToDo');
-    columnTodo.drowColumn()
+    const columnTodo = new TrelloColumn(trelloBoardEl, "ToDo");
+    columnTodo.drowColumn();
 
-    const columnProgress = new TrelloColumn(trelloBoardEl, 'Progress');
-    columnProgress.drowColumn()
+    const columnProgress = new TrelloColumn(trelloBoardEl, "Progress");
+    columnProgress.drowColumn();
 
-    const columnDone = new TrelloColumn(trelloBoardEl, 'Done');
-    columnDone.drowColumn()
+    const columnDone = new TrelloColumn(trelloBoardEl, "Done");
+    columnDone.drowColumn();
 
     this.containerEl.appendChild(trelloBoardEl);
-
   }
-
 }
---
+++ src/js/SaveToLocalStorage.js
@@ -0,0 +1,15 @@
+export default class SaveToLocalStorage {
+    static pushTask(column, task) {
+        const tasks = this.getTasks(column);
+        tasks.push(task);
+        localStorage.setItem(column, JSON.stringify(tasks));
+    }
+
+    static getTasks(column) {
+        const tasksString = localStorage.getItem(column);
+        const tasks = tasksString ? JSON.parse(tasksString) : [];
+        return tasks;
+    }
+
+    
+}
